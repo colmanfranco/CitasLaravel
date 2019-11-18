acc412a420a3aebaaf1c65e00934e73b3fd9cc59
Thierry M <dev@perif.co>
2019-11-18 12:48:34 +0100

    merging validation from franco and merging to remote origin

--- CitasLaravel/resources/views/home.blade.php
+++ CitasLaravel/resources/views/home.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="col-md-3-8">
-
-
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"><h2>Welcome {{ Auth::user()->name }}!</h2></div>
-
-                <div class="card-body" style="display: flex;flex-direction: row;justify-content: center;">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div>
-                        <a class="btn btn-warning" href="/message">Appointment List</a>
-                        <a class="btn btn-primary"  href="/message/create">Request Appointment</a>
+                @if (session('status'))
+                    <div class="alert alert-info" role="alert">
+                        <strong>{{ session('status') }}</strong>
                     </div>
+            </div>
+            @endif
+            @isset($messages)
+                <button type="button" class="btn btn-success"><a class="btn btn-success" href="/message">Appointment
+                        List</a></button>
+            @endisset
+            <div class="btn-group a" role="group">
+                @empty($messages)
+                    <button type="button" class="btn btn-dark"><a class="btn btn-dark disabled"
+                                                                  href="/message">No Appointments</a></button>
+                @endempty
+                <button type="button" class="btn btn-primary"><a class="btn btn-primary" href="/message/create">Request
+                        Appointment</a></button>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-</div>
 @endsection
--- CitasLaravel/resources/views/messageIndex.blade.php
+++ CitasLaravel/resources/views/messageIndex.blade.php
@@ -4,74 +4,74 @@
 <div class="container">
     <div class="flex-row">
         <div class="col-xs-1-12">
-            <h1 class="title">Appointment List</h1>
-@if ($messages->all() == null)
-<div class="alert alert-info alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        <span class="sr-only">Close</span>
-    </button>
-    <h1><strong> No Appointments in queue</strong></h1>
-</div>
-@endif
-@cannot('view', Message::class)
-    @foreach ($messages as $message)
-    <div class="card-columns">
-        <div class="card">
-             {{ csrf_field() }}
-            <div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{$message->subject}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted"><strong>Requested By:</strong>{{$message->username}}</h6>
-                        <h6><strong>Trainer:</strong> {{$message->requested_to}}</p></h6>
-                        <h6 class="card-subtitle mb-2 text-muted"><strong>Requested at:</strong>{{$message->created_at}}</h6>
-                            <div class="card-columns">
-                                <div class="flex-row">
-                                    @can('update', $message)
-                                    <form method="get" action="/message/{{$message->id}}/edit">
-                                        <button class="btn btn-secondary" type="submit">Edit</button>
-                                    </form>
-                                    @endcan
-                                    @can('delete', $message)
-                                    <form action="/message/{{$message->id}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                    <button class="btn btn-danger" type="submit">Delete</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-                        </div>
+
+            @if (isset($success))
+                <div class="col-sm-12">
+                    <div class="alert  alert-success alert-dismissible fade show" role="alert">
+                        <strong>$success</strong>
                     </div>
                 </div>
         </div>
-    </div>
-@endforeach
-    <div class="col-md-10 align-items-center">
-        b4
-        <a class="btn btn-primary" href="/message/create">
-        <button class="btn btn-primary">
-            Request Appointment
-        </button>
-        </a>
-            @can('deleteAll', $message)
-            <span>
-                    <form action="/all-messages" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" >
-                            <a class="btn btn-danger" type="submit">Delete All Appointments</a>
-                        </button>
-                    </form>
-            </span>
+        @endif
+            <h1 class="title">Appointment List</h1>
+@if ($messages->all() == null)
+            <div class="alert alert-info alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    <span class="sr-only">Close</span>
+                </button>
+                <h1><strong> No Appointments in queue</strong></h1>
             </div>
+@endif
+@cannot('view', Message::class)
+    <div class="card-columns">
+
+        @foreach ($messages as $message)
+        <div class="card">
+             {{ csrf_field() }}
+            <div class="card-body">
+                <h5 class="card-title">{{$message->subject}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted"><strong>Requested By:</strong>{{$message->username}}</h6>
+                <h6><strong>Trainer:</strong> {{$message->requested_to}}</h6>
+                <h6 class="card-subtitle mb-2 text-muted"><strong>Requested at:</strong>{{$message->created_at}}</h6>
+                <div class="btn-group a" role="group">
+                    @can('update', $message)
+                        <form method="get" action="/message/{{$message->id}}/edit">
+                            <button class="btn btn-secondary" type="submit">Edit</button>
+                        </form>
+                    @endcan
+                    @can('delete', $message)
+                        <form action="/message/{{$message->id}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
+                    @endcan
+                </div>
+            </div>
+        </div>
+@endforeach
+
+        <div class="btn-group" role="group">
+            <a class="btn btn-primary" href="/message/create">
+                <button class="btn btn-primary">
+                    Request Appointment
+                </button>
+            </a>
+            @can('deleteAll', $message)
+                <form action="/all-messages" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">
+                        <a class="btn btn-danger" type="submit">Delete All</a>
+                    </button>
+                </form>
         </div>
             @endcan
     </div>
     </div>
+
 </div>
 @endcannot
-        </div>
-    </div>
 @endsection
 
